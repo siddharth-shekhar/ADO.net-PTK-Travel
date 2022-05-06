9a5bcdcf0087005c37a1e71a015cdac07a9d5d6e
siddharth-shekhar <siddshekhar.12@gmail.com>
2022-05-06 16:04:16 +0000

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PKR_Travels_DatabaseFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        REDBUSEntities context = new REDBUSEntities();
+        public ActionResult Index()
+        {
+            return View(context.BusInfoes);
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PKR_Travels_DatabaseFirst.MvcApplication" Language="C#" %>
